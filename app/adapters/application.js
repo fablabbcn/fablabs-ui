35549e91e4fbd2946f38ac280e90d0626da42954
hiromipaw <silvia@nopressure.co.uk>
2016-07-28 18:45:17 +0200

    Change endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: 'http://api.fablabs.dev:8080',
+  host: 'http://fablabs.dev:8080',
   namespace:  'v1',
   authorizer: 'authorizer:fablabs',
   headers: {
