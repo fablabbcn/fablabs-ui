d9d577971f8534d6abf981eab85fdca01e5c0fb4
hiromipaw <silvia@nopressure.co.uk>
2016-07-28 23:17:44 +0200

    Change endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,8 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: 'http://fablabs.dev:8080',
-  namespace:  'v1',
+  namespace:  'api/v1',
   authorizer: 'authorizer:fablabs',
   headers: {
     Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('ember_simple_auth:session')).authenticated.token
--- app/authenticators/fablabs.js
+++ app/authenticators/fablabs.js
@@ -4,15 +4,14 @@ import Devise from 'ember-simple-auth/authenticators/devise';
 const { RSVP: { Promise }, get, run, isEmpty, $ } = Ember;
 
 export default Devise.extend({
-  serverTokenEndpoint: 'http://fablabs.dev:8080/sessions',
-  crossOriginWhitelist: ['http://fablabs.dev:8080/'],
+  serverTokenEndpoint: '/api/sessions',
   identificationAttributeName: 'email_or_username',
   tokenAttributeName: 'token',
 
   makeGetRequest() {
     return $.ajax({
       type: "GET",
-      url: "http://fablabs.dev:8080/oauth/authorize?client_id=c0015d629941172c06cdfee6744b7dd173d5398e0ea0d48b7d9e557dd43a22fa&redirect_uri=http%3A%2F%2Fweb.fablabs.dev%3A8080%2Fme&response_type=code",
+      url: "/api/oauth/authorize?client_id=c0015d629941172c06cdfee6744b7dd173d5398e0ea0d48b7d9e557dd43a22fa&redirect_uri=http%3A%2F%2Fweb.fablabs.dev%3A8080%2Fme&response_type=code",
       data: { },
       crossDomain: true,
     });
