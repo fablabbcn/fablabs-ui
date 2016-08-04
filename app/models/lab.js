464353786f7dea173231fe175b488f458c95566e
hiromipaw <silvia@nopressure.co.uk>
2016-08-04 23:44:31 +0200

    Update ui

--- app/models/lab.js
+++ app/models/lab.js
@@ -7,10 +7,32 @@ export default Model.extend({
     name:               attr('string'),
     slug:               attr('string'),
     description:        attr('string'),
-    email:              attr('string'),
+    contacts:           attr(),
     location:           attr(),
     kind:               attr('string'),
     avatar:             attr('string'),
+    header:             attr('string'),
+    users:              attr(),
+    projects:           attr(),
+    capabilities:       attr(),
+    machines:           attr(),
+
+    projects_count: Ember.computed('projects', function() {
+      return `${this.get('projects').length}`
+    }),
+
+    users_count: Ember.computed('users', function() {
+      return `${this.get('users').length}`
+    }),
+
+    machines_count: Ember.computed('machines', function() {
+      return `${this.get('machines').length}`
+    }),
+
+    lab_capabilities: Ember.computed('capabilities', function() {
+      return `${this.get('capabilities').map(function(x){ return x.replace(/_/g, ' ') } )}`.split(","); ;
+    }),
+
     lab_kind: Ember.computed('kind', function() {
       return `${this.get('kind').replace('_', '')}`;
     }),
